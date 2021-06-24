337613f128a962902c05da393f0977281cd8953e
alexandr.ivanov <alexandr.ivanov@team.wrike.com>
2021-06-24 14:25:02 +0300

    Add GitLab integration

---
+++ gitlab_auth.tf
@@ -0,0 +1,21 @@
+resource "vault_jwt_auth_backend" "gitlab" {
+  description = "JWT auth backend for gitlab integration"
+  path = "jwt"
+  type = "jwt"
+  bound_issuer = "git.example.com"
+  jwks_url = "https://git.example.com/-/jwks"
+}
+
+
+resource "vault_jwt_auth_backend_role" "qaa_tools" {
+  backend = vault_jwt_auth_backend.gitlab.path
+  role_name = "devops"
+  role_type = "jwt"
+  token_policies = [vault_policy.devops.name]
+  token_explicit_max_ttl = 60
+  user_claim = "user_email"
+  bound_claims_type = "glob"
+  bound_claims = {
+    "project_id" = "1234" # Project ID in gitlab
+  }
+}
